88cbb19deb1888c6e586ff1cdf655a48930b40a6
olga <>
2021-07-21 16:30:24 +0200

    add OwlCarousel

--- assets/script/main.js
+++ assets/script/main.js
@@ -2,6 +2,24 @@ $( document ).ready(function() {
   if ($("body").data("title") === "main" ||
   $("body").data("title") === "blog" ||
   $("body").data("title") === "account") {
+    const titlesAndAnchors = [
+      {
+        nom: "A propos de moi",
+        anchors: "index.html#main-section"
+      },
+      {
+        nom: "Gallérie",
+        anchors: "index.html#gallery-section"
+      },
+      {
+        nom: "Blog",
+        anchors: "blog.html"
+      },
+      {
+        nom: "Contact",
+        anchors: "account.html"
+      }
+    ];
 // Création de la navbar et le footer communs pour les trois pages
     const header = $('#header-navbar');
     const navbar = $(document.createElement('nav'));
@@ -11,14 +29,13 @@ $( document ).ready(function() {
     const about = $(document.createElement('section'));
     // anonymous self-invoking fonction qui crée les elements de la navbar
     (function (){
-      let titles = ["A propos de moi", "Gallerie", "Blog", "Contact"];
-      for(let i=0; i<titles.length; i++){
+      for(let i=0; i<titlesAndAnchors.length; i++){
         let individualLi = $(document.createElement('li'));
         let anchor = $(document.createElement('a'));
-        anchor.attr( {"href": "#", "class": "link-in-navbar"});
+        anchor.attr( {"href": `${titlesAndAnchors[i].anchors}`,"class": "link-in-navbar"});
         individualLi.attr({"class": "list-item "});
         $(ul).append(individualLi);
-        anchor.append(document.createTextNode(titles[i]));
+        anchor.append(document.createTextNode(titlesAndAnchors[i].nom));
         $(individualLi).append(anchor);
         $(main).append(about.attr({"id": "section-about"}));
       }
@@ -27,11 +44,79 @@ $( document ).ready(function() {
     $(navbar).append(logo);
     $(navbar).append(ul);
     $(header).append(navbar);
-      $('#header-navbar').on('click', $('.link-in-navbar'), function() {
-        $('.link-in-navbar').each(function(){
-          console.log($(this).text());
-        })
-      });
+      $('.link-in-navbar').on('mouseenter', function(){
+        console.log($(this).text());
+      })
+// Création de la section Gallérie
+    const gallerySection = $('#gallery-section');
+    class Paintings {
+      constructor(technic, name, img, info) {
+        this.technic = technic;
+        this.name = name;
+        this.img = img;
+        this.info = info;
+        }
+      }
+    const poppy = new Paintings("acryl", "Coquelicot", "assets/img/acryl-flowers.jpg", ["acrylic painting"]);
+    const mountain = new Paintings("acryl", "Village de montagne", "assets/img/acryl-mountain.jpg", ["couleurs et inspiration"]);
+    const sea = new Paintings("acryl", "Les falaises", "assets/img/acryl-sea.jpg", ["les nuances du gris et blue"]);
+    const village = new Paintings("acryl", "Village sous le soleil", "assets/img/acryl-village.jpg", ["la vivacité des couleurs"]);
+    const sunflowers = new Paintings("acryl", "Tournesols", "assets/img/acryl-sunflower.jpg", ["la beauté des contrasts"]);
+    const galleryArray = [poppy, mountain, sea, village, sunflowers];
+// // Creation de la div pour l'image principale de la caroussel
+    const secondDivInCaroussel = $(document.createElement('div'));
+    const mainDivInCaroussel = $(document.createElement('div'));
+    $(mainDivInCaroussel).attr({"class": "gtco-testimonials"})
+    $(secondDivInCaroussel).attr({"class": "owl-carousel owl-carousel1 owl-theme"});
+    $(mainDivInCaroussel).append(secondDivInCaroussel);
+    $(gallerySection).append(mainDivInCaroussel);
+// Creation des elements de la gallérie de class="acryl"
+    for(let i=0; i<galleryArray.length; i++){
+      const divPictureParent =$(document.createElement('div'));
+      $(divPictureParent).attr({"class": "card pictures-hidden"});
+      const picture = $(document.createElement('img'));
+      $(picture).attr({"class": "card-img-top", "src": `${galleryArray[i].img}`, "alt": `${galleryArray[i].name}`})
+      const spanForText = $(document.createElement('span'));
+      spanForText.append(document.createTextNode(galleryArray[i].name));
+      $(divPictureParent).append(picture);
+      $(divPictureParent).append(spanForText);
+      $(secondDivInCaroussel).append(divPictureParent);
+    }
+(function () {
+  "use strict";
+
+  var carousels = function () {
+    $(".owl-carousel1").owlCarousel({
+      loop: true,
+      center: true,
+      margin: 0,
+      responsiveClass: true,
+      nav: false,
+      responsive: {
+        0: {
+          items: 1,
+          nav: false
+        },
+        680: {
+          items: 2,
+          nav: false,
+          loop: false
+        },
+        1000: {
+          items: 3,
+          nav: true
+        }
+      }
+    });
+  };
+
+  (function ($) {
+    carousels();
+  })(jQuery);
+})();
+
+
+
 
   }
 });
